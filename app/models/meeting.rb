8613ece1d6b413f45d98bf6faac4db11b50fccb5
Patsy Price <patsy.m.price@gmail.com>
2013-07-17 09:32:13 -0700

    add comments to meeting.rb model

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -8,8 +8,12 @@ class Meeting < ActiveRecord::Base
     names[day-1]
   end
 
+# self.includes is true eagerly loading really loads everything instead of self.joins
+# self.joins is an inner join
+
   def self.cached_meeting_eagerly
-      Rails.cache.fetch([self, "all_meetings"]) { self.includes(:student, :instructor).select("meetings.*, students.name, instructors.name").all }
+      Rails.cache.fetch([self, "all_meetings"])
+      { self.includes(:student, :instructor).select("meetings.*, students.name, instructors.name").all }
   end
 
 end
